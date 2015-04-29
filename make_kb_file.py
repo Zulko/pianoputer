a1228a240208ed7d0b07f2faa93551adaacd97ba
Jiri Tyr <jiri.tyr@gmail.com>
2015-04-29 14:36:46 +0100

    Adding command line options
    This patch is adding support for command line options which can be used to
    specify the WAV and keyboard file. It also fixes some warnings from scipy
    module and improves the code formatting to be PEP8 compliant.

--- make_kb_file.py
+++ make_kb_file.py
@@ -5,18 +5,18 @@ import pygame as pg
 kb_file = open("my_keyboard.kb", 'w')
 
 pg.init()
-screen = pg.display.set_mode((200,200))
-print ("Press the keys in the right order. Press Escape to finish.")
+screen = pg.display.set_mode((200, 200))
+print("Press the keys in the right order. Press Escape to finish.")
 while True:
-	event =  pg.event.wait()
-	if event.type is pg.KEYDOWN:
-		if event.key == pg.K_ESCAPE:
-			break
-		else:
-			name = pg.key.name(event.key)
-			print("Last key pressed: %s"%name)
-			kb_file.write(name + '\n')
+    event = pg.event.wait()
+    if event.type is pg.KEYDOWN:
+        if event.key == pg.K_ESCAPE:
+            break
+        else:
+            name = pg.key.name(event.key)
+            print("Last key pressed: %s" % name)
+            kb_file.write(name + '\n')
 
 kb_file.close()
-print ("Done. you have a new keyboard configuration file: %s"%(kb_file))
+print("Done. you have a new keyboard configuration file: %s" % (kb_file.name))
 pg.quit()
